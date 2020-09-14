3975d3cb0334064c200129bc14efd7dbb1a28468
Himiko_Kuronaga <hugolanguila@gmail.com>
2020-09-14 18:44:28 -0500

    Docs agregado

---
+++ docs/precache-manifest.0729efc3887c9871f78bb2c4b3386d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abe47f3593ce3d99343dda4cef703c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ed9b750a5a8c4d3e050",
+    "url": "/static/css/main.ea583b91.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed9b750a5a8c4d3e050",
+    "url": "/static/js/main.ed4c3f05.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
